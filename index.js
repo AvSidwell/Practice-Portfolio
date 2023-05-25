bd132a70d9664388e6d5d3759b125c2fa3758a95
AvSidwell <sidwellavuyilebatyi@gmail.com>
2023-05-25 15:58:30 +0200

    commit

--- index.js
+++ index.js
@@ -3,44 +3,52 @@
 
 let project = [
     {
-      id: 1,
-      date: "2023/04/25",
-      gitHub: "https://animekaizoku.com/",
-      netlify: "This netlify",
+        id: 1,
+        image: "./Assets/images/4.jpg",
+        date: "2023/04/25",
+        gitHub: '1   ',
+        netlify: "2   ",
     },
     {
-      id: 2,
-      date: "2023/04/26",
-      gitHub: "Latest certifacte",
-      netlify: "This netlify",
+        id: 2,
+        date: "2023/04/26",
+        image: "./Assets/images/4.jpg",
+        gitHub: "1   " ,
+        netlify: "2   ",
     },
     {
-      id: 3,
-      date: "2023/04/26",
-      gitHub: "Latest certifacte",
-      netlify: "This netlify",
+        id: 3,
+        date: "2023/04/26",
+        image: "./Assets/images/4.jpg",
+        gitHub: "1   ",
+        netlify: "2",
     },
     {
-      id: 4,
-      date: "2023/05/05",
-      gitHub: "Lates",
-      netlify: "This netlify",
+        id: 4,
+        date: "2023/05/05",
+        image: "./Assets/images/4.jpg",
+        gitHub: "1   ",
+        netlify: "2   ",
     },
     {
-      id: 5,
-      date: "2023/05/12",
-      gitHub: "Latest certifacte",
-      netlify: "This netlify",
+        id: 5,
+        date: "2023/05/12",
+        image: "./Assets/images/4.jpg",
+        gitHub: "git   ",
+        netlify: "2   ",
+        img: 9,
     },
-  ];
-  
-  let dispProjects = document.querySelector(".project-sec");
-  project.forEach((data) => {
+];
+
+let dispProjects = document.querySelector(".project-sec");
+project.forEach((data) => {
     dispProjects.innerHTML += `
       <div class= "card">
-      <h4 class="display-4">${data.date}</h4>
+      <img src="${data.image}" class="img1">
       <div class="card-body">
-      <a href= class="text-white">${data.gitHub} @ <span>${data.netlify}</span></p>
+      <a href="${data.gituhub}" class="anchor btn btn-light"></a>
+      <a href="${data.netlify}" class="anchor btn btn-light"></a>
+      <p class="text-white">${data.id}</p>
       </div>
       </div>`;
-  });
+}); 
